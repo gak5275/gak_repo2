63d02d074eeabd14db33e82da660d89384ec7a2b
gak5275 <gak5275@psu.edu>
2023-01-30 13:19:27 -0500

    moved files

---
+++ PythonNLP2/Exercise1.py
@@ -0,0 +1,12 @@
+import spacy
+nlp = spacy.cli.download("en_core_web_sm")
+nlp = spacy.load('en_core_web_sm')
+
+DHshowfull = open('DHshowfull.txt', 'r', encoding='utf8')
+words = DHshowfull.read()
+wordstrings = str(words)
+print(wordstrings)
+
+DHshowfull = nlp(wordstrings)
+for token in DHshowfull:
+    print(token.text, "---->", token.pos_, ":::::", token.lemma_)
