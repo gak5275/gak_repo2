2717127b1a583a60a5bb0575f6e20773f11cae04
gak5275 <gak5275@psu.edu>
2023-10-09 16:43:26 -0400

    changed variables

--- docs/exercise1.js
+++ docs/exercise1.js
@@ -1,19 +1,19 @@
 window.addEventListener('DOMContentLoaded',init,false);
 
 function init() {
-    var buttons = document.getElementsByTagName("button")
-buttons[0].addEventListener('click', changeGreen,false)
-buttons[1].addEventListener('click', changeRed,false)
+    var buttons = document.getElementsByTagName("button");
+buttons[0].addEventListener('click', changeGreen,false);
+buttons[1].addEventListener('click', changeRed,false);
 }
 
 function changeGreen() {
-    var colorMe1 = document.getElementById("colorGreen")
+    var colorMe1 = document.getElementById("colorGreen");
     {colorMe1.style.backgroundColor = "green";
     }
 }
 
 function changeRed() {
-    var colorMe2 = document.getElementById("colorRed")
-    {colorMe2.style.backgroundColor = "red";
+    var colorMe1 = document.getElementById("colorRed");
+    {colorMe1.style.backgroundColor = "red";
     }
 }
