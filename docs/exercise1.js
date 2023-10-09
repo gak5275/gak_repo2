e94ffc2a877f40f7637906de17fdfcfa665f5515
gak5275 <gak5275@psu.edu>
2023-10-09 13:33:00 -0400

    added javascript exercise 1

---
+++ docs/exercise1.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded',init,false);
+
+function init() {
+    alert ('If you are reading this, that means the onclick message worked!');
+    var buttons = document.getElementsByTagName("button")
+buttons[0].addEventListener('click', changeColor,false)
+}
+
+function changeColor() {
+var colorMe1 = document.getElementById("colorToggle") 
+{colorMe1.style.backgroundColor = "green";}
+}
